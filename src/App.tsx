fcfbf35427bf784d3a2aa483f60c9d67425a0cd7
PPassaro <priscilla.henrique@radixeng.com.br>
2023-11-09 13:52:36 -0300

    Ajuste de pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {InformationCard} from "./components/InformationCard";
-import {GlobalStyle} from "./styles/globalStyle";
+import {GlobalStyle} from "./styles/global-style";
 
 
 export function App() {
